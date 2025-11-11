e98db57725e8f13a548dbe84d0453ca1d559cca2
Eduardo <edugonzalezsilva72@gmail.com>
2025-11-11 02:41:54 -0300

    Corrección build.sh

--- build.sh
+++ build.sh
@@ -5,5 +5,8 @@ set -o errexit
 # Instala las dependencias
 pip install -r requirements.txt
 
+# Recolecta los archivos estáticos
+python manage.py collectstatic --no-input
+
 # Corre las migraciones
 python manage.py migrate
